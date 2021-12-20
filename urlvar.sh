f89b3993b7543cf144043ca0dbccba311b3cf579
gardotd426 <54234607+gardotd426@users.noreply.github.com>
2021-12-20 13:44:02 -0500

    Update urlvar.sh

--- urlvar.sh
+++ urlvar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export URL2=$(wget -q -O -  https:// www.moddb.com/downloads/start/207799 | grep /downloads/mirror/207799 | cut -d= -f2 | cut -d'>' -f1 | cut -d';' -f1 | grep https)
+wget -q -O -  https:// www.moddb.com/downloads/start/207799 | grep /downloads/mirror/207799 | cut -d= -f2 | cut -d'>' -f1 | cut -d';' -f1 | grep https | sed 's/"//g' | wget -i /dev/stdin -O Anomaly-1.5.1.2.7z
