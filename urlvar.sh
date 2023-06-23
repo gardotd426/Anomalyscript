40165cd14b3f15bb8fbd63f46f01c6af622dfdca
gardotd426 <54234607+gardotd426@users.noreply.github.com>
2023-06-22 20:20:48 -0400

    Update urlvar.sh

--- urlvar.sh
+++ urlvar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 url="$(wget -q -O -  https:// www.moddb.com/downloads/start/207799 | grep /downloads/mirror/207799 | cut -d= -f2 | cut -d">" -f1 | cut -d";" -f1 | grep https | jq -r)"
 
-wget -q $url -O Anomaly-1.5.1.2.7z
+wget -q $url -O Anomaly-1.5.1.2.7z --show-progress
